6ec804e858e00138ea8d24597419b03ded7e5ccc
ShadeB <shadebanda2@gmail.com>
2020-04-22 18:24:05 +0200

    Move the service worker initialisation into index.html

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,3 @@
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", () => {
-		navigator.serviceWorker
-			.register("service-worker.js")
-			.then((register) => {
-				console.log("Service worker registered successfully", register);
-			})
-			.catch((err) => {
-				console.error("Failed to register service worker: ", err);
-			});
-	});
-}
-
 const filesToCache = [
 	"/",
 	"Images/buki_cali_couch_phone_ring.webp",
