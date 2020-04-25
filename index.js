66af6a5e6800c31a64ca6973533f2312acbad39a
ShadeB <shadebanda2@gmail.com>
2020-04-25 22:58:48 +0200

    Add install to home screen functionality for mobile

--- index.js
+++ index.js
@@ -10,3 +10,38 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+// Logic to let users add the site as an app to homescreen
+
+let deferredPrompt;
+const btnInstall = document.querySelector('#btnInstall');
+
+window.addEventListener('beforeinstallprompt', e => {
+
+  btnInstall.style.display = 'block';
+
+  //Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  // TODO Add logic to show the add to home screen button
+
+  // Handle add to homescreen event
+  btnInstall.addEventListener('click', e => {
+    btnInstall.display = 'none';
+
+    //Show the install message
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(choice => {
+      if (choice.outcome === 'accepted') {
+        console.log('A2HS accepted');
+      } else {
+        console.log('A2HS rejected');
+      }
+      deferredPrompt = null;
+    })
+  })
+})
